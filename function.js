649a052b6c92ad2b146325e322219f0112314b2e
Sergio Hernandez-Acosta <79073281+sergiicodes@users.noreply.github.com>
2024-10-01 01:39:10 -0500

    Add files via upload

--- function.js
+++ function.js
@@ -1,35 +1,66 @@
-<<<<<<< HEAD
 // function.js
+/*-----------------------------------------*/
 
-=======
->>>>>>> 45dc6610ceafaa6342442f3a83224da1ba5c3a6f
-// Wrap every letter in a span
-var textWrapper = document.querySelector('.ml11 .letters');
-textWrapper.innerHTML = textWrapper.textContent.replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>");
+// This does the typwriter effect for title
+'use strict';
 
-anime.timeline()
-    .add({
-        targets: '.ml11 .line',
-        scaleY: [0, 1],
-        opacity: [0.5, 1],
-        easing: "easeOutExpo",
-        duration: 700
-    })
-    .add({
-        targets: '.ml11 .line',
-        translateX: [0, document.querySelector('.ml11 .letters').getBoundingClientRect().width + 10],
-        easing: "easeOutExpo",
-        duration: 700,
-        delay: 100
-    }).add({
-        targets: '.ml11 .letter',
-        opacity: [0, 1],
-        easing: "easeOutExpo",
-        duration: 600,
-        offset: '-=775',
-        delay: (el, i) => 34 * (i + 1)
-<<<<<<< HEAD
-    })
-=======
-    })
->>>>>>> 45dc6610ceafaa6342442f3a83224da1ba5c3a6f
+function typeWriter(el) {
+    const content = el.getAttribute('data-content'); // Get the plain text
+    const textArray = content.split(''); // Split the plain text into an array
+    let formattedContent = ''; // To build the formatted content
+    el.innerHTML = ''; // Clear the HTML before starting the typewriter effect
+
+    textArray.forEach((letter, i) => {
+        setTimeout(() => {
+            // As we type, rebuild the HTML structure
+            formattedContent += letter;
+
+            // Update the HTML with the correct structure for the highlighted characters
+            el.innerHTML = formattedContent
+                .replace('in', '<span style="color: #bb86c0;">in</span>')
+                .replace('{', '<span style="color: #f1d700;">{</span>')
+                .replace('}', '<span style="color: #f1d700;">}</span>')
+                .replace('[', '<span style="color: #f1d700;">[</span>')
+                .replace(']', '<span style="color: #f1d700;">]</span>')
+                .replace('and', '<span style="color: #569cd6;">and</span>');
+        }, 95 * i);
+    });
+
+    // Loop the typewriter effect every 8 seconds
+    setInterval(() => typeWriter(el), 8000);
+}
+
+const elementEl = document.getElementById('elementEl');
+typeWriter(elementEl);
+
+/*-----------------------------------------*/
+
+// Accordion function, left side
+$(document).ready(function() {
+    var Accordion = function(el, multiple) {
+        this.el = el || {};
+        this.multiple = multiple || false;
+
+        // Variables
+        var links = this.el.find('.link');
+
+        // Bind event
+        links.on('click', {el: this.el, multiple: this.multiple}, this.dropdown);
+    }
+
+    Accordion.prototype.dropdown = function(e) {
+        var $el = e.data.el;
+        var $this = $(this),
+            $next = $this.next();
+
+        $next.slideToggle();
+        $this.parent().toggleClass('open');
+
+        if (!e.data.multiple) {
+            $el.find('.submenu').not($next).slideUp().parent().removeClass('open');
+        }
+    }   
+
+    var accordion = new Accordion($('#accordion'), false);
+});
+
